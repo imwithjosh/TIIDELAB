bd02e5a3d0059a84fce3281b011a496f88ad4a29
Tayo Oyewale <tayooyewale@gmail.com>
2021-07-08 04:44:34 +0100

    added start up files

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+function alertForm(){
+    var name = document.getElementById('name').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var phone = document.getElementById('phone').value.trim();
+
+    if(name.length!=0 && email.length!=0 && phone.length!=0 && phone.){
+        alert("Your Name is: " + name + ", Your Email is: " + email + ", Your Phone is: " + phone);
+    }else{            
+         alert("Fill All Form");
+    }
+     
+
+
+}
+ 
